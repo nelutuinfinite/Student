e1cf9b0eef435b379b607c0544e2c780a32823f1
nelutuinfinite <81085177+nelutuinfinite@users.noreply.github.com>
2026-03-03 09:18:06 +0200

    3/3/2026

---
+++ StudentMain/src/ro/ulbs/proiectaresoftware/student/Student.java
@@ -0,0 +1,36 @@
+package ro.ulbs.proiectaresoftware.student;
+
+public class Student {
+    private int numarMaricol;
+   private String nume, prenume, grupa;
+
+    public Student(int numarMaricol, String nume, String prenume, String grupa) {
+        this.numarMaricol = numarMaricol;
+        this.nume = nume;
+        this.prenume = prenume;
+        this.grupa = grupa;
+    }
+
+    public int getNumarMaricol() {
+        return numarMaricol;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public String getGrupa() {
+        return grupa;
+    }
+
+    @Override
+    public String toString() {
+         return String.format("%8d %8s %8s %8s", numarMaricol,nume,prenume,grupa);
+
+    }
+
+}
